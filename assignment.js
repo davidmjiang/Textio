4422fc0b7c2d9e29bd041aa54daa642c93e7008c
David Jiang <davidjgrey@gmail.com>
2016-12-17 17:01:40 -0800

    added basic interface

--- assignment.js
+++ assignment.js
@@ -42,7 +42,8 @@ function MinHeap(){
 			this.insert(node, this.root);
 		}
 		else{
-			if(node.count > this.root.count && this.notASubset(node)){
+			// >= makes sure that longer phrases that come after shorter phrases will pass the conditional and check for subsets
+			if(node.count >= this.root.count && this.notASubset(node)){
 				this.insert(node, this.root);
 			}
 		}
@@ -75,15 +76,24 @@ function MinHeap(){
 				return;
 			}
 			var currentNode = startAt;
-			while(currentNode.count < node.count && currentNode.next){
+			var prevNode;
+			while(node.count > currentNode.count && currentNode.next){
+				prevNode = currentNode;
 				currentNode = currentNode.next;
 			}
-			var temp = currentNode.next;
-			currentNode.next = node;
-			node.next = temp;
+			if(prevNode && currentNode.next){
+				node.next = currentNode;
+				prevNode.next = node;
+			}
+			else{
+				var temp = currentNode.next;
+				currentNode.next = node;
+				node.next = temp;
+			}
 			//remove the min if list is more than 10
 			if(this.count > 10){
 				this.root = this.root.next;
+				this.count --;
 			}	
 		}
 	};
@@ -179,8 +189,17 @@ var parse = function(line){
 var addToHeap = function(node, startAt){
 		var children = node.children;
 		while (children.length){
-			var words = startAt || [node.word];
+			var words;
+			if(startAt){
+				words = startAt.slice();
+			}
+			else{
+				words = [node.word];
+			}
 			var endOfPhrase = children.pop();
+			// if(endOfPhrase.word === "survey"){
+			// 	debugger;
+			// }
 			words.push(endOfPhrase.word);
 			if(endOfPhrase.count){
 				top10.addNode(words.join(" "), endOfPhrase.count);
@@ -203,8 +222,28 @@ var getFrequentPhrases = function(myDocument){
 	top10.print();
 };
 
-var myString = "For the past decade, Oregon’s calling card has been speed: fast players on offense, fast players on defense, and a scheme constructed to run plays faster than opponents could prepare for them. The Ducks outsprinted their Pac-12 opponents time and again, routinely finding themselves prominently mentioned in the national championship conversation.Just two years ago, quarterback Marcus Mariota won the Heisman Trophy by the second-largest margin in the award’s history and the Ducks walloped Florida State, 59–20, in the College Football Playoff semifinal. Last year they won just nine games, dropping out of championship contention quickly, and fans hoped it was a blip on the radar before the team returned to the 10-win plateau. Instead, the Ducks went 4–8 in 2016 — and it wasn’t a good 4–8, either. At least Notre Dame lost seven close games. Oregon got blown out several times.Hypothetically, Helfrich’s strength is developing quarterbacks, but instead he developed a quarterback problem. He hired college football laughingstock Brady Hoke as his defensive coordinator, and Hoke did what Hoke does best: look slightly confused while an aggressively bad thing happened under his watch. The Ducks rank 126th out of 128 FBS teams in scoring defense, never allowing fewer than 26 points in a game — not even against FCS opponent UC Davis, which also fired its coach after the season. Perhaps worst of all, Oregon lost to rival Oregon State for the first time since 2007. So the Ducks fired Helfrich and turned to Willie Taggart, who revived Western Kentucky (0–12 the season before he arrived; 7–5 when he left) and then the University of South Florida (3–9 before he arrived; 10–2 this season), to replace him. This isn’t how Oregon does things: The school hadn’t fired a head coach or hired a head coach from outside the program since 1976, a truly preposterous fact compared with the quickly spinning coaching carousel everybody else in this sport is riding.But none of this is how Oregon does things. The idea of Oregon as a consistent national championship contender is a new one. The program skyrocketed to prominence using a unique formula, and as suddenly as that formula worked, it fizzled out. No, Taggart isn’t even a tad Oregon, but he might be the best option to help Oregon maintain the level its fan base now expects.If you imagine college football’s upper tier as a country club, you can hear everybody snickering at Oregon. The Ducks are among the newest members, having only recently been granted admission. They have no national championships and half of their conference titles have come since 2000. Their rise to success is heavily linked to the rise of Nike, a multibillion-dollar corporation run by free-spending alumnus Phil Knight. They have bad etiquette out on the course — their greatest successes came under Kelly, an offensive innovator who refused to play the way that everybody else expected him to. And they committed the no. 1 sin of country-club membership — they broke the dress code. While many other powerhouses sport the same jersey designs they’ve had since they wore leather helmets, Nike makes the Ducks fly, giving them new, extremely loud uniform combos every game. Oregon isn’t old money, and doesn’t act like it. And nothing makes country-club members more angry than the nouveau riche.The Ducks have to think differently, and not just because the state of Oregon considers itself no. 1 in weirdness. To be a national title contender, a college football program pretty much has to recruit more four- and five-star prospects than two- and three-star players. The state of Oregon produces between zero and two four-star recruits in most years, and it manages only a few five-stars per decade. (The state’s last five-star prospect, per 247Sports.com’s composite rankings: running back Thomas Tyner, in 2013, who played two seasons for the Ducks before retiring due to medical reasons.)";
+var listResults = function(){
+	var currentNode = top10.root;
+		while(currentNode){
+			var phrase = $("<td></td>").text(currentNode.phrase);
+			var occurances = $("<td></td>").text(currentNode.count);
+			var resultRow = $("<tr></tr>").prepend(occurances).prepend(phrase);
+			$("#results").prepend(resultRow);
+			currentNode = currentNode.next;
+		}
+	var phraseHeader = $("<th></th>").text("Phrase");
+	var countHeader = $("<th></th>").text("Occurances");
+	var tableHeader = $("<tr></tr>").prepend(countHeader).prepend(phraseHeader);
+	$("#results").prepend(tableHeader);
+};
 
-getFrequentPhrases(myString);
+$(document).ready(function(){
+	$("#analyze").click(function(e){
+		e.preventDefault();
+		var myString = $("#input").val();
+		getFrequentPhrases(myString);
+		listResults();
+	})
+});
 
 
